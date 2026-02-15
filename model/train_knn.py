0e5f6ca00ead9186b9e24c8967725943ccc17c39
Khushboo05319 <2025aa05319@wilp.bits-pilani.ac.in>
2026-02-15 15:01:14 +0530

    Create train_knn.py

---
+++ model/train_knn.py
@@ -0,0 +1,18 @@
+import joblib
+from sklearn.datasets import load_breast_cancer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+data_obj = load_breast_cancer()
+X, y = data_obj.data, data_obj.target
+
+scaler_knn = StandardScaler()
+X_scaled = scaler_knn.fit_transform(X)
+
+X_tr, X_te, y_tr, y_te = train_test_split(X_scaled, y, test_size=0.25, random_state=11)
+
+knn_clf = KNeighborsClassifier(n_neighbors=7, metric="minkowski")
+knn_clf.fit(X_tr, y_tr)
+
+joblib.dump((knn_clf, scaler_knn), "saved_models/knn.pkl")
